f3b2e20b837b560c1bec3cce57378dd39cb7aef8
Serhii Kushnir <s1983kushnir@gmail.com>
2024-08-19 11:24:15 +0200

    Fixing Tailwind config for usin colors not only from config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 
 
 module.exports = {
+  mode: 'jit',
   content: [
     './src/**/*.html',
     './src/**/*.js',
@@ -9,33 +10,35 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
-    // Change colors here... For using only these colors
-    colors: {
-      primary: '#3498db',
-      secondary: '#f1c40f',
-      success: '#2ecc71',
-      error: '#e74c3c',
-      warning: '#f7dc6f',
-      dark: '#333',
-      light: '#f9f9f9',
-    },
-    // spacing: {
-    //   sm: '8px',
-    //   md: '16px',
-    //   lg: '24px',
-    //   xl: '32px',
-    // },
-    typography: {
-      fontFamily: {
-        sans: ['Open Sans', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
+    extend: {
+      // Change variables for colors here...
+      colors: {
+        primary: '#3498db',
+        secondary: '#f1c40f',
+        success: '#2ecc71',
+        error: '#e74c3c',
+        warning: '#f7dc6f',
+        dark: '#333',
+        light: '#f9f9f9',
       },
-      fontSize: {
-        xs: '12px',
-        sm: '14px',
-        md: '16px',
-        lg: '18px',
-        xl: '20px',
+      // spacing: {
+      //   sm: '8px',
+      //   md: '16px',
+      //   lg: '24px',
+      //   xl: '32px',
+      // },
+      typography: {
+        fontFamily: {
+          sans: ['Open Sans', 'sans-serif'],
+          serif: ['Merriweather', 'serif'],
+        },
+        fontSize: {
+          xs: '12px',
+          sm: '14px',
+          md: '16px',
+          lg: '18px',
+          xl: '20px',
+        },
       },
     },
   },
