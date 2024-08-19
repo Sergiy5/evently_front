0398ac77631776bfe052b196b494c047fd1e0f1f
Serhii Kushnir <s1983kushnir@gmail.com>
2024-08-19 16:59:40 +0200

    New files settings

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,16 @@ import js from '@eslint/js';
 import globals from 'globals';
 import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
 import typescriptEslintParser from '@typescript-eslint/parser';
-import react from 'eslint-plugin-react'; // Add react plugin
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   {
     files: ['**/*.{ts,tsx}', 'tailwind.config.js'],
-    ignores: ['dist'],
+    ignores: ['dist', 'build'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2022,
       globals: {
         ...globals.browser,
         module: 'readonly',
@@ -33,7 +33,7 @@ export default [
     },
     settings: {
       react: {
-        version: 'detect', // Automatically detect the React version
+        version: 'detect',
       },
     },
     rules: {
@@ -48,9 +48,9 @@ export default [
         'error',
         { blankLine: 'always', prev: '*', next: 'return' },
       ],
-      'react/react-in-jsx-scope': 'off', // Disable React in scope rule for JSX
-      'react/prop-types': 'off', // Disable prop-types rule for TypeScript
-      'no-undef': 'off', // Disable undefined check, should be handled by TypeScript
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'no-undef': 'off',
     },
   },
   js.configs.recommended,
@@ -58,31 +58,89 @@ export default [
 
 // import js from '@eslint/js';
 // import globals from 'globals';
+// import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+// import typescriptEslintParser from '@typescript-eslint/parser';
+// import react from 'eslint-plugin-react'; // Add react plugin
 // import reactHooks from 'eslint-plugin-react-hooks';
 // import reactRefresh from 'eslint-plugin-react-refresh';
-// import tseslint from 'typescript-eslint';
 
-// export default tseslint.config({
-//   extends: [js.configs.recommended, ...tseslint.configs.recommended],
-//   files: ['**/*.{ts,tsx}'],
-//   ignores: ['dist'],
-//   languageOptions: {
-//     ecmaVersion: 2020,
-//     globals: globals.browser,
+// export default [
+//   {
+//     files: ['**/*.{ts,tsx}', 'tailwind.config.js'],
+//     ignores: ['dist'],
+//     languageOptions: {
+//       ecmaVersion: 2020,
+//       globals: {
+//         ...globals.browser,
+//         module: 'readonly',
+//         __dirname: 'readonly',
+//         React: true,
+//       },
+//       parser: typescriptEslintParser,
+//       sourceType: 'module',
+//     },
+//     parserOptions: {
+//       project: ['./tsconfig.node.json', './tsconfig.app.json'],
+//       tsconfigRootDir: import.meta.dirname,
+//     },
+//     plugins: {
+//       react: react,
+//       'react-hooks': reactHooks,
+//       'react-refresh': reactRefresh,
+//       '@typescript-eslint': typescriptEslintPlugin,
+//     },
+//     settings: {
+//       react: {
+//         version: 'detect', // Automatically detect the React version
+//       },
+//     },
+//     rules: {
+//       ...typescriptEslintPlugin.configs.recommended.rules,
+//       ...react.configs.recommended.rules,
+//       ...reactHooks.configs.recommended.rules,
+//       'react-refresh/only-export-components': [
+//         'warn',
+//         { allowConstantExport: true },
+//       ],
+//       'padding-line-between-statements': [
+//         'error',
+//         { blankLine: 'always', prev: '*', next: 'return' },
+//       ],
+//       'react/react-in-jsx-scope': 'off', // Disable React in scope rule for JSX
+//       'react/prop-types': 'off', // Disable prop-types rule for TypeScript
+//       'no-undef': 'off', // Disable undefined check, should be handled by TypeScript
+//     },
 //   },
-//   plugins: {
-//     'react-hooks': reactHooks,
-//     'react-refresh': reactRefresh,
-//   },
-//   rules: {
-//     ...reactHooks.configs.recommended.rules,
-//     'react-refresh/only-export-components': [
-//       'warn',
-//       { allowConstantExport: true },
-//     ],
-//   },
-//   'padding-line-between-statements': [
-//     'error',
-//     { blankLine: 'always', prev: '*', next: 'return' },
-//   ],
-// });
+//   js.configs.recommended,
+// ];
+
+// // import js from '@eslint/js';
+// // import globals from 'globals';
+// // import reactHooks from 'eslint-plugin-react-hooks';
+// // import reactRefresh from 'eslint-plugin-react-refresh';
+// // import tseslint from 'typescript-eslint';
+
+// // export default tseslint.config({
+// //   extends: [js.configs.recommended, ...tseslint.configs.recommended],
+// //   files: ['**/*.{ts,tsx}'],
+// //   ignores: ['dist'],
+// //   languageOptions: {
+// //     ecmaVersion: 2020,
+// //     globals: globals.browser,
+// //   },
+// //   plugins: {
+// //     'react-hooks': reactHooks,
+// //     'react-refresh': reactRefresh,
+// //   },
+// //   rules: {
+// //     ...reactHooks.configs.recommended.rules,
+// //     'react-refresh/only-export-components': [
+// //       'warn',
+// //       { allowConstantExport: true },
+// //     ],
+// //   },
+// //   'padding-line-between-statements': [
+// //     'error',
+// //     { blankLine: 'always', prev: '*', next: 'return' },
+// //   ],
+// // });
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   build: {
-    outDir: 'build',
+    outDir: 'build', // Adjust to your output directory
   },
   plugins: [react()],
   css: {
@@ -14,10 +14,34 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
-      components: `${path.resolve(__dirname, './src/components/')}`,
-      public: `${path.resolve(__dirname, './public/')}`,
+      components: path.resolve(__dirname, './src/components/'),
+      public: path.resolve(__dirname, './public/'),
       pages: path.resolve(__dirname, './src/pages'),
-      types: `${path.resolve(__dirname, './src/@types')}`,
+      types: path.resolve(__dirname, './src/@types'),
     },
   },
 });
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+
+// export default defineConfig({
+//   build: {
+//     outDir: 'build',
+//   },
+//   plugins: [react()],
+//   css: {
+//     postcss: './postcss.config.js',
+//   },
+//   base: '/evently_front/',
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src/'),
+//       components: `${path.resolve(__dirname, './src/components/')}`,
+//       public: `${path.resolve(__dirname, './public/')}`,
+//       pages: path.resolve(__dirname, './src/pages'),
+//       types: `${path.resolve(__dirname, './src/@types')}`,
+//     },
+//   },
+// });
