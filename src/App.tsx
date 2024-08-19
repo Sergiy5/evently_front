ed04580d117807e1d49c9f5f471b9e8aee6de251
Serhii Kushnir <s1983kushnir@gmail.com>
2024-08-19 15:13:48 +0200

    Renovation dist

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layaout';
+import Home from './pages/Home';
 // import NotFoundPage from './pages/NotFoundPage';
 
-const Home = React.lazy(() => import('./pages/Home'));
+// const Home = React.lazy(() => import('./pages/Home'));
 
 const App: React.FC = () => {
   return (
