84ad3c475d7965adcce78603fbf8bbf5c6b81dfc
Serhii Kushnir <s1983kushnir@gmail.com>
2024-08-19 22:20:51 +0200

    Added / to base_name in index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="evently_front">
+    <BrowserRouter basename="/evently_front">
       <App />
     </BrowserRouter>
   </React.StrictMode>
