bd85fda2fc7d7c7b5a4b7bc749d262bc7db918e3
McLareni <mukola.kosobutski05@gmail.com>
2024-08-31 12:00:38 +0200

    refactoryFile

---
+++ src/components/events/EventsList.tsx
@@ -0,0 +1,20 @@
+import EventCard from './EventCard';
+import { eventType } from './types';
+
+interface props {
+  events: eventType[];
+}
+
+const EventsList: React.FC<props> = ({ events }) => {
+  return (
+    <ul>
+      {events.map((event: eventType) => (
+        <li key={event.id}>
+          <EventCard event={event}/>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default EventsList;
