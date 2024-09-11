f0a58192ccb61aa7a2af738688ce6574b7dcaef0
McLareni <mukola.kosobutski05@gmail.com>
2024-09-11 11:31:42 +0200

    Merge branch 'UIKit'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-
 export default {
   mode: 'jit',
   content: [
@@ -11,8 +10,30 @@ export default {
   ],
   theme: {
     extend: {
+      // Font family imported
+      fontFamily: {
+        oswald: ['Oswald', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'],
+        lora: ['Lora', 'serif'],
+      },
+
       // Change variables for colors here...
       colors: {
+        // Main color
+        lightBlue: '#D7EBFF',
+        lightPurple: '#E2DEFF',
+        lightGreen: '#CFFFCF',
+        lightPink: '#FFD6F7',
+        buttonPurple: '#9B8FF3',
+
+        //Neutral color
+        background: '#F8FBFF',
+        lightGray: '#F4F4F4',
+        gray: '#E8EAEC',
+        darkGray: '#D0D5D8',
+        textDark: '#062341',
+
+        // Other color
         primary: '#3498db',
         secondary: '#f1c40f',
         success: '#2ecc71',
