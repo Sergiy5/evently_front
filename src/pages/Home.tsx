ffcf4a34778e68f54d567bb5c59eb51f2c69bd5e
Serhii Kushnir <s1983kushnir@gmail.com>
2024-09-20 13:56:21 +0200

    Remove auth and modal from home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import { Auth, Modal } from "@/components";
 import { Main } from "@/components/Main";
 import { Container } from "@/components/Container";
 import { TopEvents } from "@/components/TopEvents";
@@ -7,14 +5,6 @@ import { AllEvents } from "@/components/AllEvents";
 import { Hero } from "@/components/Hero";
 
 const Home: React.FC = () => {
-  const  [isOpen, setIsOpen ] = useState<boolean>(false);
-
-  const loginUser= () => {
-   setIsOpen(true);
-  }
-  const onCloseModal = () => {
-    setIsOpen(false);
-  }
 
   return (
     <Main>
@@ -23,18 +13,9 @@ const Home: React.FC = () => {
       <AllEvents />
       <Container>
         <div className="h-auto">
-          <button
-            id="register"
-            className="bg-blue-500 hover:bg-blue-700 focus:outline-none text-white font-bold py-2 px-4 rounded"
-            onClick={loginUser}
-            >
-            Auth
-          </button>
+          
           </div>
       </Container>
-        <Modal isOpen={isOpen} onClose={onCloseModal}>
-          <Auth onCloseModal={onCloseModal} />
-        </Modal>
     </Main>
   );
 };
