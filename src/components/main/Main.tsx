d080d427320d66a56ccb28886da7b5f5d2c91d16
Yevhenns <gifestum@gmail.com>
2024-11-02 12:02:15 +0200

    hero slider

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,16 +1,18 @@
-import { MainLines } from "./MainLines";
+import { MainLines } from './MainLines';
 
 interface MainProps {
   children: React.ReactNode;
   className?: string;
 }
+
 export const Main: React.FC<MainProps> = ({ children, className }) => {
-  
   return (
-    <main
-      className='relative'
-    >
-      <div className={`relative z-20 flex flex-col gap-16 pt-[60px] max-w-[1440px] ${className}`}>{children}</div>
+    <main className="relative">
+      <div
+        className={`relative z-20 flex flex-col gap-16 pt-[34px] max-w-[1440px] ${className}`}
+      >
+        {children}
+      </div>
       <MainLines />
     </main>
   );
