82474b9a5d942121c161969fab47bde6635f669e
Yevhenns <gifestum@gmail.com>
2024-11-02 14:01:06 +0200

    top fix

---
+++ src/components/topEvents/MySliderBtn.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import clsx from 'clsx';
+import { MdKeyboardArrowLeft } from 'react-icons/md';
+
+export interface CustomArrowProps {
+  next?: boolean;
+  currentSlide?: number;
+  slideCount?: number;
+}
+
+export const MySliderBtn: React.FC<CustomArrowProps> = ({
+  next = false,
+  currentSlide = 0,
+  slideCount = 0,
+  ...props
+}): React.JSX.Element => {
+  const isActiveNext = currentSlide > slideCount - 5;
+  const isActivePrev = currentSlide === 0;
+
+  const isActive = next ? isActiveNext : isActivePrev;
+
+  return (
+    <button
+      {...props}
+      className={clsx(
+        `absolute -top-20 flex items-center justify-center w-[40px] h-[40px] rounded-full 
+         focus:outline-none transition-colors transition-outline duration-250 ease-[cubic-bezier(0.4,0,0.2,1)] z-10`,
+        {
+          'cursor-default': isActive,
+          'cursor-pointer': !isActive,
+          [`${
+            !isActive
+              ? 'text-background bg-borderColor '
+              : 'text-textDark bg-gray'
+          }`]: true,
+        },
+        next ? 'right-16' : 'right-[125px]'
+      )}
+    >
+      <MdKeyboardArrowLeft
+        className={`ml-auto mr-auto text-inherit w-10 h-10 ${next && 'rotate-180'}`}
+      />
+    </button>
+  );
+};
--- src/components/topEvents/TopEvents.tsx
+++ src/components/topEvents/TopEvents.tsx
@@ -1,23 +1,28 @@
-import { Settings } from 'react-slick';
-import { MySliderBtn } from '../ui/MySliderBtn';
-import { MySlider } from '../ui/MySlider';
+import { MySliderBtn } from './MySliderBtn';
 import { Container } from '../container/Container';
 import { EventCard } from '../ui';
 import { IEvent } from '@/types/components';
-
+import { useRef } from 'react';
+import { nanoid } from '@reduxjs/toolkit';
+import Slider, { Settings } from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 interface TopEventsProps {
   filteredEvents: IEvent[] | undefined;
 }
+
 export const TopEvents: React.FC<TopEventsProps> = ({ filteredEvents }) => {
+  const sliderRef = useRef<Slider | null>(null);
+
   const settings: Settings = {
     pauseOnHover: true,
-    slidesToShow: 4.25,
+    slidesToShow: 4.15,
     slidesToScroll: 4,
     speed: 2000,
     infinite: false,
-    nextArrow: <MySliderBtn next_style={true} />,
     prevArrow: <MySliderBtn />,
+    nextArrow: <MySliderBtn next />,
     arrows: true,
     pauseOnFocus: true,
     initialSlide: 0,
@@ -45,16 +50,17 @@ export const TopEvents: React.FC<TopEventsProps> = ({ filteredEvents }) => {
   };
 
   return (
-    <div className={`flex flex-col gap-8`}>
+    <div>
       <Container>
-        <h1>Топ подій</h1>
+        <h1 className="mb-[32px]">Топ подій</h1>
       </Container>
-
-      <MySlider
-        arraySlides={filteredEvents}
-        SlideComponent={EventCard}
-        settings={settings}
-      />
+      <div className="pl-[60px] position-relative">
+        <Slider ref={sliderRef} {...settings}>
+          {filteredEvents?.map(item => (
+            <EventCard key={nanoid()} event={item} />
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
