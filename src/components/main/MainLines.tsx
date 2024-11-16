e5ce5a3b662df48c6f06649dd1898f4d8deda64f
McLareni <mukola.kosobutski05@gmail.com>
2024-11-16 18:07:13 +0100

    fixed header, fix lines

--- src/components/main/MainLines.tsx
+++ src/components/main/MainLines.tsx
@@ -5,27 +5,19 @@ export const MainLines: React.FC = () => {
     <>
       <div
         key={nanoid()}
-        className={`absolute z-10 h-[1px] translate-x-[-50%] left-[50%] bg-lightPurple top-0 w-lvw`}
+        className={`absolute z-50 w-[1px] top-0 bg-lightPurple left-5 h-full`}
       ></div>
       <div
         key={nanoid()}
-        className={`absolute z-10 h-[1px]  translate-x-[-50%] left-[50%] bg-lightPurple top-5 w-lvw`}
+        className={`absolute z-50 w-[1px] top-0 bg-lightPurple left-10 h-full`}
       ></div>
       <div
         key={nanoid()}
-        className={`absolute z-10 w-[1px] top-0 bg-lightPurple left-5 h-full`}
+        className={`absolute z-50 w-[1px] top-0 bg-lightPurple right-5 h-full`}
       ></div>
       <div
         key={nanoid()}
-        className={`absolute z-10 w-[1px] top-0 bg-lightPurple left-10 h-full`}
-      ></div>
-      <div
-        key={nanoid()}
-        className={`absolute z-10 w-[1px] top-0 bg-lightPurple right-5 h-full`}
-      ></div>
-      <div
-        key={nanoid()}
-        className={`absolute z-10 w-[1px] top-0 bg-lightPurple right-10 h-full`}
+        className={`absolute z-50 w-[1px] top-0 bg-lightPurple right-10 h-full`}
       ></div>
     </>
   );
