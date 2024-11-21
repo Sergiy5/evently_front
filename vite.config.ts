ab661bd1e5517c7c3d5968ebb0b14b0e8d93d16f
McLareni <139352135+McLareni@users.noreply.github.com>
2024-11-21 23:26:27 +0100

    Revert "Fix SPA routing"

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/evently_front/',
+  base: '/evently_front',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'), // Set up path alias for `@` to `./src/`
